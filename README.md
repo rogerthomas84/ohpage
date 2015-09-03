aa1e28500bb1fb8bc6cc7d4285736c149c2e9331
Roger Thomas <rogere84@gmail.com>
2015-09-03 09:06:20 +0100

    Readme changes

--- README.md
+++ README.md
@@ -22,6 +22,7 @@ To use OhPage with Composer, add the dependency (and version constraint) to your
 }
 ```
 
+
 Quick Start
 -----------
 
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,14 @@
 <?php
-include dirname(__FILE__) . '/../vendor/autoload.php';
+$dir = realpath(__DIR__ . '/../');
+spl_autoload_register(
+    function($class)
+    {
+        $dir = realpath(__DIR__ . '/../src');
+        $target = explode('\\', $class);
+        $path = $dir . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $target) . '.php';
+        if (file_exists($path)) {
+            require_once $path;
+        }
+    }
+);
+
